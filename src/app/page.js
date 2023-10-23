e1fb964206dae7e3c54a029fd2ef7e254caa5262
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-23 18:28:56 -0500

    Cambios

--- src/app/page.js
+++ src/app/page.js
@@ -11,16 +11,16 @@ export default function Home() {
 
   return (
     <>
-      <div className='relative'>
+      {/* <div className='relative'>
         <div className="absolute top-[-75px] left-[-100px]">
           <Image src={Images.img.aiBg} alt="Lia" />
         </div>
         <div className="absolute top-[-35px] right-[-60px]">
           <Image src={Images.img.adBg} objectFit="cover" alt="Lia" />
         </div>     
-      </div>
+      </div> */}
       
-      <main className={`flex flex-col min-w-screen items-center pt-24 pb-5 px-5 bg-[#fcf6f1]`}>
+      <main className={`flex flex-col min-w-screen items-center pt-24 pb-5 px-5 bg-[#fff1e5]`}>
 
         <div className='relative'>
           <div className="w-[270px] h-[270px] ml-[30px] mt-[7px] overflow-hidden z-0 absolute">
@@ -31,8 +31,8 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <h2 className='text-center mt-[35px] text-[#240B06]'>Mi bautizo</h2>
-          <h1 className='text-center mt-[35px] '>Lia Paez</h1>
+          <h2 className='text-center mt-[35px] text-[#240B06]'>MI BAUTIZO</h2>
+          <h1 className='text-center mt-[35px]'>Lia Violeta Paez Rojas</h1>
           <p className='text-center ms:max-w-full md:max-w-[600px] mt-[35px] text-[#240B06] ms:text-[15px] md:text-[20px]'>Señor hoy me presentaran ante ti para ser bañada con la gracia de tu amor, toma mi pequeño corazón entre tus benditas manos y jamás te separes de mi.</p>
         </div>
           <div className="">
@@ -60,14 +60,10 @@ export default function Home() {
               <p className='ms:text-[15px] md:text-[20px]'>Jeferson Paez</p>
             </div>
           </div>
-          <div className='flex flex-col justify-center items-center mt-[35px]'>
-            <h2 className='text center mb-[20px]'>LLUVIA DE SOBRES</h2>
-            <Image src={Images.img.sobre} width={100} height={100} alt="Sobre" />
-          </div>
-          <h3 className='text-center mt-[35px] text-[30px]'>Ubicación</h3>
-          <div className='flex justify-around  mt-[35px]'>
+          <h3 className='text-center mt-[35px] text-[30px]'>UBICACIÓN</h3>
+          <div className='flex justify-between  mt-[35px]'>
             <div className='flex flex-col items-center justify-center max-w-[140px]'>
-              <h3>Misa</h3>
+              <h4>Misa</h4>
                 <div className='flex justify-center items-center'>
                   <button className="border-[#A67D4B] border-2 text-white p-4 rounded-full flex items-center justify-center m-5">
                   <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"
@@ -78,7 +74,7 @@ export default function Home() {
               <span className=' text-center'>parroquia san pedro de usme</span><br/>
             </div>
             <div className='flex flex-col items-center justify-center max-w-[140px] mt-[18px]'>
-                <h3>Recepción</h3>
+                <h4>Recepción</h4>
                 <div>
                   <button className="border-[#A67D4B] border-2 text-white p-4 rounded-full flex items-center justify-center m-5">
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" className="w-8 h-8"
@@ -88,7 +84,7 @@ export default function Home() {
               <span className=' text-center'> Finca famir paez (vereda chiguaza-usme)</span><br/>
             </div>
           </div>
-          <h3 className='text-center mt-[35px] text-[30px]'>Asistencia</h3>
+          <h3 className='text-center mt-[10px] text-[30px]'>ASISTENCIA</h3>
           <div className='flex flex-col items-center justify-center '>
                 <div>
                   <Link
@@ -103,6 +99,10 @@ export default function Home() {
                   className='w-8 h-8'><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
                   </Link>
                 </div>
+          <div className='flex flex-col justify-center items-center mt-[35px]'>
+            <h2 className='text center mb-[20px]'>LLUVIA DE SOBRES</h2>
+            <Image src={Images.img.sobre} width={100} height={100} alt="Sobre" />
+          </div>
             </div>
             {/* <div className="bottom-0 left-0 right-0  overflow-hidden">
               <div className="absolute ms:bottom-[-950px] md:bottom-[-970px] left-[-50px]">
