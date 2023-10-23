143d6529567bd37252d459be034e2296ec777fea
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-23 09:48:00 -0500

    Cargue

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,49 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hola</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="relative">
+        <div className="w-32 h-32 rounded-full overflow-hidden">
+          <Image src="/public/borde.png" layout="fill" objectFit="cover" alt="Borde" />
+        </div>
+        <div className="w-32 h-32 rounded-full overflow-hidden -mt-32">
+          <Image src="/public/lia.jpg" layout="fill" objectFit="cover" alt="Lia" />
+        </div>
+      </div>
+      <div>
+        <p className='text-center max-w-xs'>Tenemos el gusto de invitarte al bautizo de nuestra pequeña hija</p>
+        <h1 className='text-center'>Lia Paez</h1>
+      </div>
+        <div className="">
+          <div className="flex items-center justify-between">
+            <div className='border-t border-b'>Sábado</div>
+            <div className='flex justify-center items-center flex-col'>
+              <div>25 </div>
+              <div>Diciembre</div>
+            </div>
+            <div className='border-t border-b'>10:00am</div>
+          </div>
+        <div className="flex">
+          <div className="w-1/3">
+            <h3>Papás:</h3>
+            <p>Aledxa Rojas</p>
+          </div>
+          <Image src="/papas.jpg" width={100} height={100} alt="Papás" />
+          <div className="w-1/3">
+            <h3>Padrinos:</h3>
+          </div>
+        </div>
+        <div>
+          <button style={{ background: '#F2B6C6' }}>Ubicación</button>
+          <button style={{ background: '#F288AF' }}>Asistencia</button>
+          <i className="fab fa-whatsapp" style={{ color: '#A67D4B' }}></i>
+        </div>
+        <div>
+          <p>Lluvia de sobres</p>
+          <Image src="/sobre.jpg" width={100} height={100} alt="Sobre" />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/assets/img/index.js
@@ -0,0 +1,16 @@
+import adBg from "./img/ad-bg.png"
+import aiBg from "./img/ai-bg.png"
+import bdBg from "./img/bi-bi.png"
+
+
+import border from './img/borde.png'
+import sobre from './img/sobre.png'
+import paloma from './img/paloma.png'
+
+// Exporta todas las imágenes organizadas en un objeto
+export const Images = {
+    img:{
+        LogoWhite,
+        LogoGreen
+    },
+}
