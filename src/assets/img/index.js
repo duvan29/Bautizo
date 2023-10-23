48e95a422fbe060b8c81c748c3863d9aa1f28a4b
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-23 09:50:16 -0500

    Cambios

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,16 +1,25 @@
 import adBg from "./img/ad-bg.png"
 import aiBg from "./img/ai-bg.png"
 import bdBg from "./img/bi-bi.png"
+import bi from './img/bd-bg.png'
 
 
 import border from './img/borde.png'
 import sobre from './img/sobre.png'
 import paloma from './img/paloma.png'
+import lia from './img/lia.jpg'
+
 
 // Exporta todas las imágenes organizadas en un objeto
 export const Images = {
     img:{
-        LogoWhite,
-        LogoGreen
+        adBg,
+        aiBg,
+        bdBg,
+        bi,
+        border,
+        sobre,
+        paloma,
+        lia,
     },
 }
