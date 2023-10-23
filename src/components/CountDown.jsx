3b766402dfa36926290eaf043eebf10cdcdad3ec
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-23 12:29:02 -0500

    Contador

---
+++ src/components/CountDown.jsx
@@ -0,0 +1,77 @@
+"use client"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function CountDown () {
+    const [partyTime, setPartyTime] = useState(false);
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+  
+    useEffect(() => {
+      const target = new Date("11/16/2023 23:59:59");
+  
+      const interval = setInterval(() => {
+        const now = new Date();
+        const difference = target.getTime() - now.getTime();
+  
+        const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+        setDays(d);
+  
+        const h = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        setHours(h);
+  
+        const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        setMinutes(m);
+  
+        const s = Math.floor((difference % (1000 * 60)) / 1000);
+        setSeconds(s);
+  
+        if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+          setPartyTime(true);
+        }
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+  return (
+    <>
+        {partyTime ? (
+            <>
+              <h1>Happy new year!</h1>
+              <video autoPlay loop muted>
+                <source src="/party.mp4" />
+              </video>
+            </>
+          ) : (
+            <>
+            <div className="border-[#A67D4B] p-4 rounded-lg text-center my-[30px] border-2">
+                <div className="flex items-center justify-between">
+                    <div className="timer-segment flex flex-col">
+                        <span className="time">{days}</span>
+                        <span className="label">Días</span>
+                    </div>
+                    <div className="timer-segment flex flex-col">
+                        <span className="time">{hours}</span>
+                        <span className="label">Horas</span>
+                    </div>
+                    <div className="timer-segment flex flex-col">
+                        <span className="time">{minutes}</span>
+                        <span className="label">Minutos</span>
+                    </div>
+                    <div className="timer-segment flex flex-col">
+                        <span className="time">{seconds}</span>
+                        <span className="label">Segundos</span>
+                    </div>
+                </div>
+            </div>
+            </>
+          )}
+
+    </>
+  );
+}
