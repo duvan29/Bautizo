48986037600f9060cb2194d310473e7adb31ae1e
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-31 13:08:03 -0500

    CAMBIOS

--- src/app/page.js
+++ src/app/page.js
@@ -27,12 +27,12 @@ export default function Home() {
         </div>
           <div className="">
             <div className="flex items-center justify-between mt-[35px]">
-              <h2 className='border-t-2 border-b-2 md:text-[30px] ms:text-[20px] border-[#F288AF] '>SABADO</h2>
+              <h2 className='border-t-2 border-b-2 md:text-[30px] min-w-[120px] text-center ms:text-[20px] border-[#F288AF] '>SABADO</h2>
               <div className='flex justify-center items-center flex-col mx-[20px]'>
                 <h2 className='md:text-[40px] ms:text-[35px]'>11</h2>
                 <h2 className='md:text-[30px] ms:text-[25px]'>Noviembre</h2>
               </div>
-              <h2 className='border-t-2 border-b-2 md:text-[30px] ms:text-[20px] border-[#F288AF]'>7:00AM</h2>
+              <h2 className='border-t-2 border-b-2 md:text-[30px] min-w-[120px] text-center ms:text-[20px] border-[#F288AF]'>7:00AM</h2>
             </div>
             <CountDown/>
           <div className="flex justify-evenly mt-[35px] md:w-[450px] ms:w-full">
@@ -61,7 +61,7 @@ export default function Home() {
                   fill="#A67D4B"><path d="M344 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V48H264c-13.3 0-24 10.7-24 24s10.7 24 24 24h32v46.4L183.3 210c-14.5 8.7-23.3 24.3-23.3 41.2V512h96V416c0-35.3 28.7-64 64-64s64 28.7 64 64v96h96V251.2c0-16.9-8.8-32.5-23.3-41.2L344 142.4V96h32c13.3 0 24-10.7 24-24s-10.7-24-24-24H344V24zM24.9 330.3C9.5 338.8 0 354.9 0 372.4V464c0 26.5 21.5 48 48 48h80V273.6L24.9 330.3zM592 512c26.5 0 48-21.5 48-48V372.4c0-17.5-9.5-33.6-24.9-42.1L512 273.6V512h80z"/></svg>
                   </div>
                 </div>
-              <span className=' text-center'>Parroquia san pedro de usme</span><br/>
+              <span className=' text-center'>Parroquia San Pedro De Usme</span><br/>
               <h4 className='border-t-2 border-b-2 border-[#A67D4B]'>7:00AM</h4>
             </div>
             <div className='flex flex-col items-center justify-center md:max-w-[180px] ms:max-w-[250px] ms:mt-[18px] md:mt-0'>
